ec2d3661d0b64a81a3f9fb8e97fc3d2fbde68dc5
kiwan33 <rumansillar@gmail.com>
2024-11-19 12:51:25 +0100

    Add button component

---
+++ src/lib/Button.svelte
@@ -0,0 +1,24 @@
+<script>
+    
+</script>
+
+<button class="Btn">Iniciar Sesión</button>
+
+<style>
+    .Btn {
+        font-size: 18px;
+        border: none;
+        cursor: pointer;
+        color: white;
+        background-color: var(--color-azul);
+        width: 100%;
+        height: 40px;
+        border-radius: 5px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+    .Btn:focus {
+        outline: none;
+    }
+</style>
